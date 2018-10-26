4cf5932ddc0e9db9a35810eea4d91d7d77eb2931
Bram De Coninck <bram@bramdeconinck.com>
2018-10-26 21:47:40 +0200

    Added a first draft of the models

---
+++ uwp-app-aalst-groep-a3/Models/Merchant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uwp_app_aalst_groep_a3.Models
+{
+    public class Merchant : User
+    {
+        public int MerchantId { get; set; }
+        public Category Category { get; set; }
+        public List<String> Tags { get; set; }
+        public List<Company> Companies { get; set; }
+        //sociale media
+    }
+}
---
+++ uwp-app-aalst-groep-a3/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uwp_app_aalst_groep_a3.Models
+{
+    abstract class User
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+    }
+}
