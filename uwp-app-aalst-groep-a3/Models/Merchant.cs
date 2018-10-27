e54db9f16d27fcb6ef7944f5375ce3000ec5777c
Bram De Coninck <bram@bramdeconinck.com>
2018-10-27 15:06:57 +0200

    More adjustements to the models

--- uwp-app-aalst-groep-a3/Models/Merchant.cs
+++ uwp-app-aalst-groep-a3/Models/Merchant.cs
@@ -11,6 +11,6 @@ namespace uwp_app_aalst_groep_a3.Models
         public List<Category> Categories { get; set; }
         public List<String> Tags { get; set; }
         public List<Company> Companies { get; set; }
-        public Dictionary<SocialMediaEnum, String> SocialMedia { get; set; }
+        //sociale media
     }
 }
