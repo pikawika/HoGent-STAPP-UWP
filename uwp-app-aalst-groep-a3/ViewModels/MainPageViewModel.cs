cf7da924c6b87e3c0b88c062227ef3a35b244028
Bram De Coninck <bram@bramdeconinck.com>
2018-10-27 16:35:30 +0200

    Started working on the MainPage GUI

--- uwp-app-aalst-groep-a3/ViewModels/MainPageViewModel.cs
+++ uwp-app-aalst-groep-a3/ViewModels/MainPageViewModel.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using uwp_app_aalst_groep_a3.Utils;
+using Windows.UI.Xaml.Controls;
 
 namespace uwp_app_aalst_groep_a3.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public Frame MainFrame { get; set; }
+        public RelayCommand ShowEventsCommand { get; set; }
 
+        private ViewModelBase _currentData;
+
+        public ViewModelBase CurrentData
+        {
+            get { return _currentData; }
+            set { _currentData = value; RaisePropertyChanged(); }
+        }
+
+
+        public MainPageViewModel()
+        {
+            ShowEventsCommand = new RelayCommand(_ => ShowEvents());
+        }
+
+        private void ShowEvents()
+        {
+            //CurrentData = new MoviesViewModel();
+        }
     }
 }
