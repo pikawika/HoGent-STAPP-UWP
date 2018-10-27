7b79ad97565d493385d0820397293f4c6508d802
jodidl1998 <jodi.deloof.y0180@student.hogent.be>
2018-10-27 22:55:46 +0200

    homepage - basic bottom recomended companies

--- uwp-app-aalst-groep-a3/Views/HomePageView.xaml.cs
+++ uwp-app-aalst-groep-a3/Views/HomePageView.xaml.cs
@@ -26,5 +26,10 @@ namespace uwp_app_aalst_groep_a3.Views
         {
             this.InitializeComponent();
         }
+
+        private void Search_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
     }
 }
