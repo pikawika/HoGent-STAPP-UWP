f2099acac0977af940a0de62403f1670d24cce9c
Bram De Coninck <bram@bramdeconinck.com>
2018-10-27 12:31:55 +0200

    Applied the MVVM architecture to the project

---
+++ uwp-app-aalst-groep-a3/ViewModels/ViewModelBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace uwp_app_aalst_groep_a3.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        private DataTemplate _template;
+
+        public DataTemplate Template
+        {
+            get { return _template; }
+            set { _template = value; RaisePropertyChanged("Template"); }
+        }
+
+        public ViewModelBase()
+        {
+            Template = GetTemplate();
+        }
+
+        private DataTemplate GetTemplate()
+        {
+            string s = GetType().Name; //bvb. MainPageViewModel
+            return (DataTemplate)App.Current.Resources[s]; //bvb. View:MainPage
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
