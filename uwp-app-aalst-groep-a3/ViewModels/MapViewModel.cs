7251577ce9cc1b428769dd0773abae56ca655917
Bram De Coninck <bram@bramdeconinck.com>
2018-11-12 12:45:52 +0100

    Started working on the map with merchants

--- uwp-app-aalst-groep-a3/ViewModels/MapViewModel.cs
+++ uwp-app-aalst-groep-a3/ViewModels/MapViewModel.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 
 namespace uwp_app_aalst_groep_a3.ViewModels
 {
     public class MapViewModel : ViewModelBase
     {
+        public BasicGeoposition AalstPosition { get; }
+        public Geopoint AalstPoint { get; }
+        public Double MapZoomlevel { get; }
 
+        public MapViewModel()
+        {
+            AalstPosition = new BasicGeoposition()
+            {
+                Latitude = 50.937753,
+                Longitude = 4.040881,
+            };
+
+            AalstPoint = new Geopoint(AalstPosition);
+
+            MapZoomlevel = 14.5;
+        }
     }
 }
--- uwp-app-aalst-groep-a3/Views/MapView.xaml.cs
+++ uwp-app-aalst-groep-a3/Views/MapView.xaml.cs
@@ -13,18 +13,29 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace uwp_app_aalst_groep_a3.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MapView : UserControl
     {
         public MapView()
         {
             this.InitializeComponent();
+
+            //BasicGeoposition beginPositie = new BasicGeoposition();
+            //beginPositie.Latitude = startPositie.Coordinate.Latitude;
+            //beginPositie.Longitude = startPositie.Coordinate.Longitude;
+
+            //Geopoint gp1 = new Geopoint(beginPositie);
+
+            //MapIcon mapIcon1 = new MapIcon();
+            //mapIcon1.Location = gp1;
+            //mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+            //mapIcon1.Title = "Huidige locatie!";
+            //mapIcon1.ZIndex = 0;
+            //MyMap.MapElements.Add(mapIcon1);
+
+            //MyMap.Center = gp1;
+            //MyMap.ZoomLevel = 10;
         }
     }
 }
