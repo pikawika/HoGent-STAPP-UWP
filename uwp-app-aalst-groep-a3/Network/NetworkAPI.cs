10ab40a7c0332632bfabc61fb0d8e44e474cae62
Bram De Coninck <bram@bramdeconinck.com>
2018-11-13 16:21:17 +0100

    Created REST API for retrieving establishments and promotions from the database

---
+++ uwp-app-aalst-groep-a3/Network/NetworkAPI.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using uwp_app_aalst_groep_a3.Models;
+using uwp_app_aalst_groep_a3.Models.Domain;
+
+namespace uwp_app_aalst_groep_a3.Network
+{
+    public static class NetworkAPI
+    {
+        // The HttpClient used by all methods
+        private static HttpClient client { get; } = new HttpClient();
+
+        // The base URL of our backend
+        private static String baseUrl { get; } = "http://localhost:53128/api";
+
+        /* ESTABLISHMENTS */
+        // Get all establishments
+        public static async Task<ObservableCollection<Establishment>> GetAllEstablishments()
+        {
+            ObservableCollection<Establishment> establishments = new ObservableCollection<Establishment>();
+            try
+            {
+                var json = await client.GetStringAsync(new Uri($"{baseUrl}/establishment"));
+                establishments =  JsonConvert.DeserializeObject<ObservableCollection<Establishment>>(json);
+            }
+            catch (HttpRequestException e)
+            {
+                Debug.WriteLine($"Er is een error opgetreden tijdens het" +
+                                $"ophalen van alle establishments uit de databank:" +
+                                $"{e}");
+            }
+            return establishments;
+        }
+
+        // Get establishment by id
+        public static async Task<Establishment> GetEstablishmentById(int id)
+        {
+            Establishment establishment = new Establishment();
+            try
+            {
+                var json = await client.GetStringAsync(new Uri($"{baseUrl}/establishment/{id}"));
+                establishment = JsonConvert.DeserializeObject<Establishment>(json);
+            }
+            catch (HttpRequestException e)
+            {
+                Debug.WriteLine($"Er is een error opgetreden tijdens het" +
+                                $"ophalen van een specifiek establishment uit de databank:" +
+                                $"{e}");
+            }
+            return establishment;
+        }
+
+        /* PROMOTIONS */
+        // Get all promotions
+        public static async Task<ObservableCollection<Promotion>> GetAllPromotions()
+        {
+            ObservableCollection<Promotion> promotions = new ObservableCollection<Promotion>();
+            try
+            {
+                var json = await client.GetStringAsync(new Uri($"{baseUrl}/promotion"));
+                promotions = JsonConvert.DeserializeObject<ObservableCollection<Promotion>>(json);
+            }
+            catch (HttpRequestException e)
+            {
+                Debug.WriteLine($"Er is een error opgetreden tijdens het" +
+                                $"ophalen van alle promotions uit de databank:" +
+                                $"{e}");
+            }
+            return promotions;
+        }
+
+        // Get promotion by id
+        public static async Task<Promotion> GetPromotionById(int id)
+        {
+            Promotion promotion = new Promotion();
+            try
+            {
+                var json = await client.GetStringAsync(new Uri($"{baseUrl}/promotion/{id}"));
+                promotion = JsonConvert.DeserializeObject<Promotion>(json);
+            }
+            catch (HttpRequestException e)
+            {
+                Debug.WriteLine($"Er is een error opgetreden tijdens het" +
+                                $"ophalen een specifieke promotion uit de databank:" +
+                                $"{e}");
+            }
+            return promotion;
+        }
+
+    }
+}
--- uwp-app-aalst-groep-a3/ViewModels/HomePageViewModel.cs
+++ uwp-app-aalst-groep-a3/ViewModels/HomePageViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using uwp_app_aalst_groep_a3.Models;
+using uwp_app_aalst_groep_a3.Models.Domain;
+using uwp_app_aalst_groep_a3.Network;
 
 namespace uwp_app_aalst_groep_a3.ViewModels
 {
     public class HomePageViewModel : ViewModelBase
     {
+        public ObservableCollection<Promotion> Promotions { get; set; }
+        public ObservableCollection<Establishment> Establishments { get; set; }
+
+        public HomePageViewModel()
+        {
+            InitializeHomePage();
+        }
+
+        private async void InitializeHomePage()
+        {
+            Promotions = await NetworkAPI.GetAllPromotions();
+            Establishments = await NetworkAPI.GetAllEstablishments();
+        }
+
     }
 }
