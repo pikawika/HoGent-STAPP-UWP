4dd3aeb63cbfee6cb5d566c32c79d56c4f7b1525
Lennert Bontinck <lennertbontinck@live.be>
2018-12-11 23:30:05 +0100

    Viewmodels

---
+++ rest-api-windows-project/Models/ViewModels/Event/AddEventViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using stappBackend.Models.ViewModels.Establishment;
+
+namespace stappBackend.Models.ViewModels.Event
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        public int? establishmentId { get; set; }
+
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        [DataType(DataType.Upload)]
+        public IFormCollection Images { set; get; }
+
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        [DataType(DataType.Upload)]
+        public IFormCollection Attachments { set; get; }
+    }
+}
---
+++ rest-api-windows-project/Models/ViewModels/Event/ModifyEventViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace stappBackend.Models.ViewModels.Event
+{
+    public class ModifyEventViewModel
+    {
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        [DataType(DataType.Upload)]
+        public IFormCollection Images { set; get; }
+
+        [DataType(DataType.Upload)]
+        public IFormCollection Attachments { set; get; }
+    }
+}
