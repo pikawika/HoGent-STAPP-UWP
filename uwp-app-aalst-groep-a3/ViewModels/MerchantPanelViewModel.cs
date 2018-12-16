6d7631966a398dcb558342a3309d8bca58fd744d
Bram De Coninck <bram@bramdeconinck.com>
2018-12-16 22:10:57 +0100

    Created a basic layout for the merchant panel

--- uwp-app-aalst-groep-a3/ViewModels/MerchantPanelViewModel.cs
+++ uwp-app-aalst-groep-a3/ViewModels/MerchantPanelViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using uwp_app_aalst_groep_a3.Base;
+using uwp_app_aalst_groep_a3.Models;
+using uwp_app_aalst_groep_a3.Network;
+using uwp_app_aalst_groep_a3.Utils;
 
 namespace uwp_app_aalst_groep_a3.ViewModels
 {
@@ -11,9 +15,81 @@ namespace uwp_app_aalst_groep_a3.ViewModels
     {
         private MainPageViewModel mainPageViewModel;
 
+        private NetworkAPI NetworkAPI = new NetworkAPI();
+
+        private ObservableCollection<Establishment> _establishments;
+
+        public ObservableCollection<Establishment> Establishments
+        {
+            get { return _establishments; }
+            set { _establishments = value; RaisePropertyChanged(nameof(Establishments)); }
+        }
+
+        private ObservableCollection<Promotion> _promotions;
+
+        public ObservableCollection<Promotion> Promotions
+        {
+            get { return _promotions; }
+            set { _promotions = value; RaisePropertyChanged(nameof(Promotions)); }
+        }
+
+        private ObservableCollection<Event> _events;
+
+        public ObservableCollection<Event> Events
+        {
+            get { return _events; }
+            set { _events = value; RaisePropertyChanged(nameof(Events)); }
+        }
+
+        public RelayCommand EstablishmentClickedCommand { get; set; }
+
+        public RelayCommand PromotionClickedCommand { get; set; }
+
+        public RelayCommand EventClickedCommand { get; set; }
+
         public MerchantPanelViewModel(MainPageViewModel mainPageViewModel)
         {
             this.mainPageViewModel = mainPageViewModel;
+
+            EstablishmentClickedCommand = new RelayCommand((object args) => EstablishmentClicked(args));
+
+            PromotionClickedCommand = new RelayCommand((object args) => PromotionClicked(args));
+
+            EventClickedCommand = new RelayCommand((object args) => EventClicked(args));
+
+            InitializeHomePage();
         }
+
+        private async void InitializeHomePage()
+        {
+            Establishments = new ObservableCollection<Establishment>(await NetworkAPI.GetSubscriptions());
+
+            var promotionList = new List<Promotion>();
+            var eventList = new List<Event>();
+
+            foreach (Establishment s in Establishments)
+            {
+                foreach (Promotion p in s.Promotions)
+                {
+                    p.Establishment = s;
+                    promotionList.Add(p);
+                }
+
+                foreach (Event e in s.Events)
+                {
+                    e.Establishment = s;
+                    eventList.Add(e);
+                }
+            }
+
+            Promotions = new ObservableCollection<Promotion>(promotionList);
+            Events = new ObservableCollection<Event>(eventList);
+        }
+
+        private void EstablishmentClicked(object args) => mainPageViewModel.NavigateTo(new EstablishmentDetailViewModel(args as Establishment, mainPageViewModel));
+
+        private void PromotionClicked(object args) => mainPageViewModel.NavigateTo(new PromotionDetailViewModel(args as Promotion, mainPageViewModel));
+
+        private void EventClicked(object args) => mainPageViewModel.NavigateTo(new EventDetailViewModel(args as Event, mainPageViewModel));
     }
 }
