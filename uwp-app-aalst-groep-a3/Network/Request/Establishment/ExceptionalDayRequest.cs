1b2b03a558a7d2dcb59ad41bf250fd37f7fc5e5c
Lennert Bontinck <lennertbontinck@live.be>
2018-12-17 18:37:36 +0100

    Company en establishment networkapi

---
+++ uwp-app-aalst-groep-a3/Network/Request/Establishment/ExceptionalDayRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace uwp_app_aalst_groep_a3.Network.Request.Establishment
+{
+    public class ExceptionalDayRequest
+    {
+        public DateTime Day { get; set; }
+        public string Message { get; set; }
+    }
+}
