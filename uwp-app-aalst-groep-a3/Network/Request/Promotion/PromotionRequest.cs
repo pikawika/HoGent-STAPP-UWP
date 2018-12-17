79d37b9b7df9315d9a8abcd10d1b04ed1c9d3319
Lennert Bontinck <lennertbontinck@live.be>
2018-12-17 20:12:27 +0100

    Event en promotion networkapi

---
+++ uwp-app-aalst-groep-a3/Network/Request/Promotion/PromotionRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using uwp_app_aalst_groep_a3.Network.Request.Attachments;
+
+namespace uwp_app_aalst_groep_a3.Network.Request.Promotion
+{
+    public class PromotionRequest
+    {
+        public int EstablishmentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public List<FileRequest> Images { get; set; }
+
+        public List<FileRequest> Attachments { get; set; }
+    }
+}
